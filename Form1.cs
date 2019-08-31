9e0cf6f53990f973ecc19173039abe31448db06f
Alvin Lee <alvin_lee5@hotmail.com>
2019-08-31 16:02:35 -0400

    First commit

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace AutoPoint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            PowerPoint.Application pptApplication = new PowerPoint.Application();
+            
+            // invisible by default
+            pptApplication.Visible = Office.MsoTriState.msoTrue;   
+
+            PowerPoint.Presentations pptPres = pptApplication.Presentations;
+            PowerPoint.Presentation pres = pptPres.Add(Office.MsoTriState.msoTrue);
+            PowerPoint.Slides pptSlides = pres.Slides;
+            PowerPoint.Slide pptSlide = pptSlides.Add(1, PowerPoint.PpSlideLayout.ppLayoutText);
+            PowerPoint.Shapes pptShapes = pptSlide.Shapes;
+            PowerPoint.Shape pptShape = pptShapes[1];
+            PowerPoint.TextFrame pptTextFrame = pptShape.TextFrame;
+            PowerPoint.TextRange pptTextRange = pptTextFrame.TextRange;
+            pptTextRange.Text = "All-In-One Code Framework";
+
+            string fileName = "C:/Users/Alvin/Desktop/sample.pptx";
+            pres.SaveAs(fileName, PowerPoint.PpSaveAsFileType.ppSaveAsOpenXMLPresentation, Office.MsoTriState.msoTriStateMixed);
+            pres.Close();
+            pptApplication.Quit();
+            Marshal.FinalReleaseComObject(pptApplication);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        }
+    }
+}
