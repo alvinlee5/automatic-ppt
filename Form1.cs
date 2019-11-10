dec40967e25cbaddc91d4f9ed270b9f5a0fb1583
Alvin Lee <alvin_lee5@hotmail.com>
2019-11-09 23:27:11 -0500

    Remove old, uneeded code

--- Form1.cs
+++ Form1.cs
@@ -32,39 +32,6 @@ namespace AutoPoint
         {
             m_powerPointManager.CreateSlide("title", "lyrics");
             m_powerPointManager.SavePowerPoint("C:/Users/Alvin/Desktop/sample.pptx");
-
-/*            PowerPoint.Application pptApplication = new PowerPoint.Application();
-            
-            // invisible by default
-            //pptApplication.Visible = Office.MsoTriState.msoTrue;   
-
-            PowerPoint.Presentations pptPres = pptApplication.Presentations;
-            PowerPoint.Presentation pres = pptPres.Add(Office.MsoTriState.msoFalse);
-            PowerPoint.Slides pptSlides = pres.Slides;
-            PowerPoint.Slide pptSlide = pptSlides.Add(1, PowerPoint.PpSlideLayout.ppLayoutBlank);
-            PowerPoint.Shapes pptShapes = pptSlide.Shapes;
-            pptShapes.AddTextbox(Office.MsoTextOrientation.msoTextOrientationHorizontal, 50, 50, 100, 100);
-            PowerPoint.Shape pptShape = pptShapes[1];
-            PowerPoint.TextFrame pptTextFrame = pptShape.TextFrame;
-            PowerPoint.TextRange pptTextRange = pptTextFrame.TextRange;
-            pptTextRange.Text = "Song title";
-
-            pptShapes.AddTextbox(Office.MsoTextOrientation.msoTextOrientationHorizontal, 100, 100, 500, 500);
-            PowerPoint.Shape pptShape2 = pptShapes[2];
-            PowerPoint.TextFrame pptTextFrame2 = pptShape2.TextFrame;
-            PowerPoint.TextRange pptTextRange2 = pptTextFrame2.TextRange;
-            pptTextRange2.Text = "Lyrics go here";
-
-            string fileName = "C:/Users/Alvin/Desktop/sample.pptx";
-            pres.SaveAs(fileName, PowerPoint.PpSaveAsFileType.ppSaveAsOpenXMLPresentation, Office.MsoTriState.msoTriStateMixed);
-            pres.Close();
-            pptApplication.Quit();
-            // TODO: Need to figure out how to close ppt application programmatically
-            Marshal.FinalReleaseComObject(pptApplication);
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
-            GC.WaitForPendingFinalizers();*/
         }
 
         private void button2_Click(object sender, System.EventArgs e)
